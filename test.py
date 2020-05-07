4896c94c8dc744aa569ed08017c20763bf222fd0
NIX369 <43085650+NIX369@users.noreply.github.com>
2020-05-07 23:45:53 +0530

    Basic_Usage_of_urllib

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-import socket
-
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))       # 80 - port to connect to
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
-
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode())
-
-mysock.close()
+import urllib.request, urllib.parse, urllib.error
+fhand = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_499385.html')
+for line in fhand:
+    print(line.decode().strip())
